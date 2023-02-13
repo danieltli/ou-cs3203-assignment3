9132e8d9940a3ac8beaaaaabf3595e480ca9f643
Daniel Li <78183445+danieltli@users.noreply.github.com>
2023-02-12 18:25:20 -0600

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -18,13 +18,3 @@ def multiply_all (numList):
 
         
     return ans
-
-def main():
-    nums = input("Enter the numbers you would like to add and multiply together, separated by commas. \n")
-    numlist = list(map(int,nums.split(',')))
-    print('sum: ', sum_all(numlist),'\n','product: ', multiply_all(numlist),'\n',sep='')
-    
-    
-
-
-main()
